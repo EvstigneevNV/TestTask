e606d7dc3a4e8cdf40428ecce8f0885bf2ad4467
EvstigneevNV <evstigneevnv@answer-42.ru>
2025-05-23 20:02:20 +0300

    Попытка завести js

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -7,8 +7,6 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.example.model.TelegramUser;
 import org.example.model.TelegramUserDetails;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
